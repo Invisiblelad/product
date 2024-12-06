0a4155ddb528837104b495855c523c7c06680cc0
Sharath Veerapaneni <140253698+Invisiblelad@users.noreply.github.com>
2024-12-06 15:29:27 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ echo "Building the Docker image..."
 docker build -t product_image .
 
 echo "Running the Docker container..."
-docker run -d -p 8080:8080 -e MONGO_URI=mongodb://172.23.212.112:27017 --name product_container product_image
+docker run -d -p 8080:8080 -e MONGO_URI=mongodb://<IP>:27017 --name product_container product_image
 
 docker ps | grep product_container
 
